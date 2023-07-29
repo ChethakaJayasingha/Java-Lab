b1060d50e1574393a32489b48e2163a59f1baa47
Chethaka Jayasingha <131871327+ChethakaJayasingha@users.noreply.github.com>
2023-07-29 10:43:54 +0530

    Update Question3.java

--- Lab10-Q3/Question3.java
+++ Lab10-Q3/Question3.java
@@ -1,11 +1,21 @@
-package com.mycompany.question3;
-public class Question3 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Question3 {
     public static void main(String[] args) {
-        
-        try {
-          File file = new File ("nonexistent.txt");
-          Scanner scanner = new Scanner(file)
+
+        try{
+            File f1=new File("\"C:\\Users\\chethaka\\Downloads\\JAVA\\Lab10\\Question3\\Java.txt\"");
+            Scanner w=new Scanner(f1);
+
+            while (w.hasNextLine()){
+                String l=w.nextLine();
+                System.out.println(l);
+            }
+            w.close();
+        }catch(FileNotFoundException a){
+            System.out.println(a.getMessage());
         }
     }
 }
