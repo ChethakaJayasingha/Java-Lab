3c6cc911eb478cc46af903584c605696187256e2
Chethaka Jayasingha <131871327+ChethakaJayasingha@users.noreply.github.com>
2023-07-29 10:36:21 +0530

    Update Question2.java

--- Lab10-Q2/Question2.java
+++ Lab10-Q2/Question2.java
@@ -2,13 +2,21 @@ package com.mycompany.question2;
 public class Question2 {
 
     public static void main(String[] args) {
-        int [] numbers = {1,2,3,4,5};
-        
-        try {
-          int values = numbers [1];
+        int[] numbers={1,24,34,56,41,98,62,43};
+        try{
+            numbers[3]=89;
+            System.out.println("Result is "+numbers[3]);
+            int ans=numbers[2]/0;
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Error: Array Index Out Of Bounds Exception");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
         }
-        catch{
-          
+        finally{
+            for(int i=0;i< numbers.length;i++){
+                System.out.println(numbers[i]);
+            }
         }
     }
 }
+
